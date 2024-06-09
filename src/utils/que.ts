a99f7667a57c2c4ceaca7976339a2cd33673a2d7
Ampoi <tange.toshihiro@outlook.jp>
2024-06-09 23:12:48 +0900

    Queクラスを作成した

---
+++ src/utils/que.ts
@@ -0,0 +1,20 @@
+export class Que {
+    constructor(
+        private readonly func: Function,
+        private readonly delay: number
+    ){}
+
+    private que: number | undefined = undefined
+
+    public add(){
+        if( this.que ) clearTimeout(this.que)
+        this.que = setTimeout(() => {
+            clearTimeout(this.que)
+            this.func()
+        }, this.delay)
+    }
+
+    public delete(){
+        if( this.que ) clearTimeout(this.que)
+    }
+}
