17e6d0e0f2fd9df028d41a18cec5a246a82d5f64
Ampoi <tange.toshihiro@outlook.jp>
2024-06-16 22:15:28 +0900

    inputコンポーネントの作成

---
+++ src/components/modal/code/input.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="relative">
+        <button
+            class="flex flex-row gap-1.5 items-baseline bg-zinc-900 border-zinc-700 border-[1px] rounded-md px-1 py-1 select-none"
+            @click="showInputEditorModal = !showInputEditorModal">
+            <div class="size-4 bg-blue-500 rounded-[4px] text-xs grid place-content-center">
+                {{ input.type[0].toUpperCase() }}
+            </div>
+            <p class="text-sm leading-none">{{ input.name }}</p>
+        </button>
+        <div
+            v-if="showInputEditorModal"
+            class="flex flex-col gap-2 absolute -bottom-1 translate-y-full z-20 left-1/2 -translate-x-1/2 bg-zinc-900 border-[1px] border-zinc-700 rounded-xl p-1.5">
+            <div class="flex flex-row gap-1.5 items-center">
+                <div class="size-6 bg-blue-500 rounded-lg grid place-content-center">
+                    {{ input.type[0].toUpperCase() }}
+                </div>
+                <input
+                    type="text"
+                    class="rounded-lg w-24 bg-transparent border-zinc-700 border-[1px] text-sm px-1 py-0.5 outline-none">
+            </div>
+            <button class="bg-red-500/30 text-sm text-red-500 font-semibold py-0.5 rounded-lg">
+                削除
+            </button>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const showInputEditorModal = ref(false)
+
+defineProps<{
+    input: {
+        name: string
+        type: string
+    }
+}>()
+</script>
--- src/components/modal/code/inputs.vue
+++ src/components/modal/code/inputs.vue
@@ -1,24 +1,19 @@
 <template>
     <div class="flex flex-row items-center gap-2">
         <h2 class="text-lg font-semibold basis-14">入力</h2>
-        <div class="grow bg-black border-[1px] border-zinc-700 px-1.5 py-1 flex flex-row gap-1.5 rounded-lg overflow-hidden">
+        <div class="grow bg-black border-[1px] border-zinc-700 px-1.5 py-1 flex flex-row gap-1.5 rounded-lg">
             <button
                 class="bg-zinc-900 border-zinc-700 border-[1px] size-6 grid rounded-md place-content-center text-sm box-content"
                 @click="addInput">
                 <Icon icon="fluent:add-16-filled"/>
             </button>
             <div
-                class="flex flex-row grow gap-1.5 overflow-x-auto"
+                class="flex flex-row grow gap-1.5 flex-wrap"
                 style="scrollbar-width: none;"
                 id="inputs">
-                <button
+                <Input
                     v-for="input in inputs"
-                    class="flex flex-row gap-1.5 items-baseline bg-zinc-900 border-zinc-700 border-[1px] rounded-md px-1 py-1 select-none">
-                    <div class="size-4 bg-blue-500 rounded-[4px] text-xs grid place-content-center">
-                        {{ input.type[0].toUpperCase() }}
-                    </div>
-                    <p class="text-sm leading-none">{{ input.name }}</p>
-                </button>
+                    :input/>
             </div>
         </div>
     </div>
@@ -32,6 +27,7 @@
 import { Icon } from "@iconify/vue/dist/iconify.js";
 import { codeData } from "../../../hooks/code"
 import { computed } from "vue";
+import Input from "./input.vue";
 
 const inputs = computed({
     get(){
